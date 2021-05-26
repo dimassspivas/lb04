f3832725d5ef5cae217f689d7b2221e3667c3f70
dimassspivas <77319264+dimassspivas@users.noreply.github.com>
2021-05-26 19:05:08 +0300

    Add files via upload

---
+++ lb04.cpp
@@ -0,0 +1,27 @@
+﻿
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	int x = rand() % 11;
+	int y;
+	{
+		while (true)
+		{
+			cout << "you: ";
+			cin >> y;
+			cout << '\n';
+			{
+				if (y < x)
+					cout << "Bot: Too Low'\n'";
+				else if (y == x)
+					cout << "Bot: Correct\n";
+				else if (y > x)
+					cout << "Bot: Too High'\n'";
+				else
+					cout << "Enter A Integer'\n'";
+			}
+		}
+	}
+}
